d4f183f09aacfc73f8d16b1bc94a812fb0c7b4fc
danieloffei519-star <danieloffei519@gmail.com>
2026-02-24 15:53:51 -0800

    Update author and index in week2_main.cpp

--- week2_main.cpp
+++ week2_main.cpp
@@ -1,7 +1,7 @@
 /*
  * ELECTRICAL LOAD MONITORING SYSTEM
- * Author: LOVE FOBI
- * Index: 01240239D
+ * Author: DANIEL OFFEI DODOO
+ * Index: 01240058D
  * Course: EEE 227
  * Week 2: Energy Calculation Added
  * Date: February 11, 2026
